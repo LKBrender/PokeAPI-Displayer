8491089b69d9dd1b79b6a710834b45686f77bde3
Lauren Brender <108022822+LKBrender@users.noreply.github.com>
2023-01-15 10:32:56 +0000

    updates to appearance and typescript planning.

--- PokeAPI-Displayer/src/app/app.component.ts
+++ PokeAPI-Displayer/src/app/app.component.ts
@@ -1,19 +1,30 @@
 import { Component } from '@angular/core';
 import { PokemonClient } from 'pokenode-ts';
+
+const PokeList = "https://pokeapi.co/api/v2/pokemon?limit=151"
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'PokeAPI-Displayer';
 }
-
 (async () => {
   const api = new PokemonClient();
 
   await api
-    .getPokemonByName(`PokeList`)
+    .getPokemonByName(PokeList)
     .then((data) => console.log(data.name))
     .catch((error) => console.error(error));
 })();
+
+//loop to get pokemon & links into array
+var i = 0;
+var PokeArray =[];
+if(i<=151){
+
+}
+
